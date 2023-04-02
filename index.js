e1ab61b036cf74cfc3db67c5bbd31c6bf60dfd02
Jackson Gravino <jacksonjlgravino@gmail.com>
2023-04-02 11:33:47 -0300

    adicionando pagina de gestao

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+var session = require('express-session')
+const fileupload = require('express-fileupload')
+const fs = require('fs')
 
 const path = require('path')
 
 const app = express()
 
 const Posts = require('./posts.js')
-const userName = ''
-const collectionName = ''
+const userName = 'root'
+const collectionName = 'dankicode'
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 }
+  })
+)
 
 mongoose
   .connect(
@@ -28,6 +38,13 @@ mongoose
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'temp')
+  })
+)
 
 app.set('view engine', 'ejs')
 app.use('/public', express.static(path.join(__dirname, 'public')))
@@ -132,6 +149,76 @@ app.get('/:slug', (req, res) => {
     .catch(err => {
       console.log(err.message)
     })
+})
+
+var usuarios = [
+  {
+    login: 'jackson',
+    senha: '123456'
+  }
+]
+
+app.post('/admin/login', (req, res) => {
+  usuarios.map(function (val) {
+    if (val.login == req.body.login && val.senha == req.body.senha) {
+      req.session.login = 'JackBoy'
+    }
+  })
+  res.redirect('/admin/login')
+})
+
+app.post('/admin/cadastro', (req, res) => {
+  let formato = req.files.arquivo.name.splite('.')
+  var imagem = ''
+  if (formato[formato.length - 1] == 'jpg') {
+    imagem = new Date().getTime + '.jpg'
+    req.files.arquivo.mv(__dirname + '/public/images/' + imagem)
+  } else {
+    fs.unlinkSync(req.files.arquivo.tempFilePath)
+  }
+
+  Posts.create({
+    titulo: req.body.titulo_noticia,
+    imagem: 'http://localhost:3000/public/images/' + imagem,
+    categoria: 'Nenuma',
+    conteudo: req.body.noticia,
+    slug: req.body.slug,
+    autor: 'Admin',
+    views: 0
+  })
+  res.redirect('/admin/login')
+})
+
+app.get('/admin/deletar/:id', (req, res) => {
+  Posts.deleteOne({ _id: req.params.id }).then(function () {
+    res.redirect('/admin/login')
+  })
+})
+
+app.get('/admin/login', (req, res) => {
+  if (req.session.login == null) {
+    res.render('admin-login')
+  } else {
+    Posts.find({})
+      .sort({ _id: -1 })
+      .then(posts => {
+        posts = posts.map(function (val) {
+          return {
+            id: val.id,
+            titulo: val.titulo,
+            conteudo: val.conteudo,
+            descricaoCurta: val.conteudo.substr(0, 100),
+            imagem: val.imagem,
+            slug: val.slug,
+            categoria: val.categoria
+          }
+        })
+        res.render('admin-painel', { posts: posts })
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
 })
 
 app.listen(3000, () => {
